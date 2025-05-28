0e9ca78137e3a5ef3bbcd9d389a8a942f5e7e0fa
Alan Varghese <alanvz2212@example.com>
2025-05-28 23:48:35 +0530

    Updated

--- lib/pages/Drawer/Navigate_Pages/privay_policy_page.dart
+++ lib/pages/Drawer/Navigate_Pages/privay_policy_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:musaliarapp/utils/colors.dart';
 import 'package:musaliarapp/vibration/vibration_helper.dart';
 
 class PrivayPolicyPage extends StatelessWidget {
