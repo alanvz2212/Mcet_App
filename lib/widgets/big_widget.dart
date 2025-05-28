ad9947eb0c32c36a4885d6ad8cd170aa337a7d5e
Alan Varghese <alanvz2212@example.com>
2025-05-28 23:49:01 +0530

    Updated

--- lib/widgets/big_widget.dart
+++ lib/widgets/big_widget.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:musaliarapp/utils/colors.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class BigWidget extends StatefulWidget {
